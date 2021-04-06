b663eef092bb1707e1cdc50e2e9c184ad1d07cd6
mmarat01 <mijaelms@protonmail.com>
2021-04-06 00:36:20 -0400

    QUICK PUSH

--- besttimetobuyandsellstock.js
+++ besttimetobuyandsellstock.js
@@ -2,5 +2,28 @@
    maximize gain by choosing 1 day to buy, 1 future day to sell
    -> O: maximum profit, if not possible, zero */
 
-//
-const maxProfit = (prices) => {};
+// 1: badbadnotgood
+const bruteMaxProfit = (prices) => {
+  let max = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; i < prices.length; j++) {
+      let diff = prices[j] - prices[i];
+      max = diff > max ? diff : max;
+    }
+  }
+  return max;
+};
+
+// 2: better approach
+const maxProfit = (prices) => {
+  let minValue = Number.MAX_VALUE;
+  let max = 0;
+  for (const [i, value] of prices.entries()) {
+    if (value < minValue) {
+      minValue = value;
+    } else if (value - minValue > max) {
+      max = value - minValue;
+    }
+  }
+  return value;
+};
